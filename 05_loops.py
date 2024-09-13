7edd0f51743ee35f32460a7c26007f92054e8308
Md. Asiful Islam <Saeedasif267@gmail.com>
2024-09-13 19:22:52 +0600

    05_loops.py

---
+++ 05_loops.py
@@ -0,0 +1,11 @@
+# Loops in Python
+
+# For loop
+for i in range(1, 6):
+    print(f"For loop iteration: {i}")
+
+# While loop
+count = 1
+while count <= 5:
+    print(f"While loop iteration: {count}")
+    count += 1
